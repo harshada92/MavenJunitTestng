99406ec605d50f9bd37b2d1e3a15a20cc6f97fb1
Harshada Hinge <harshada@bytzsoft.com>
2026-08-12 15:09:23 +0530

    test 12 aug

--- src/test/java/Bytzsoft/ClientLoginTestNG.java
+++ src/test/java/Bytzsoft/ClientLoginTestNG.java
@@ -186,7 +186,7 @@ public class ClientLoginTestNG{
         loginPage.logout();
        
 
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         }
 
      /*   File scr = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
